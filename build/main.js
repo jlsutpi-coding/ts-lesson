f08d662c038364400bab9837825437f67948767f
Jl Sut Pi <sutpi070@gmail.com>
2025-11-29 15:10:04 +0700

    ts/ls-1 init

---
+++ build/main.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+let a = 12;
+let b = "6";
+let c = 2;
+let hi = "hi";
+export {};
+// console.log(a / b);
+// console.log(c * b);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+let a: number = 12;
+let b: string = "6";
+
+let c: number = 2;
+
+let hi = "hi";
+
+// console.log(a - b);
+// console.log(a / b);
+// console.log(c * b);
